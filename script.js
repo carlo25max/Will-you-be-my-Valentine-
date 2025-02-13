c69e8a2ddc5ea041595cce1243b7651c4f3a4a50
carlo25max <castiblancocarlos447@gmail.com>
2025-02-13 16:31:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "estas segura?",
+    "realmente segura??",
+    "estas se acuerdo??",
+    "Kateee por favor...",
+    "solo piensalo!",
+    "si tu dices que no,me sentire muuy mal...",
+    "me siento tan tristee...",
+    "me siento muy pero muy triste...",
+    "esta bien, dejaré de preguntar...",
+    "solo bromeo, di si por favor! ❤️"
 ];
 
 let messageIndex = 0;
